6e84907ea3c95a2ca55223b9f140cc3758ee5d14
GabiAbacaxi <gabyolisan07@gmail.com>
2026-08-05 12:13:30 -0500

    criando a pasta components + componente home do servidor + migração do formulario para um component próprio

---
+++ estrategia-app/src/app/components/formulario-iniciativas/formulario-iniciativas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-iniciativas',
+  imports: [],
+  templateUrl: './formulario-iniciativas.html',
+  styleUrl: './formulario-iniciativas.scss',
+})
+export class FormularioIniciativas {}
