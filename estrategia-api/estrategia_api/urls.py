dafcb4c5b5b7af493f1d6291d2b075ef64499037
Andrey <andreymarques958@gmail.com>
2026-08-24 16:52:06 -0300

    feat: implementa login com interface simples

--- estrategia-api/estrategia_api/urls.py
+++ estrategia-api/estrategia_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import( TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('api/', include('projetosEstrategicos.urls')),
     path('api/', include('iniciativasEstrategicas.urls')),
     path('api/', include('indicadoresEstrategicos.urls')),
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- estrategia-app/src/app/app.routes.ts
+++ estrategia-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { FormularioIniciativas } from './components/formularios/formulario-iniciativas/formulario-iniciativas';
 import { ListagemIniciativas } from './components/listagem-iniciativas/listagem-iniciativas';
 import { Home } from './components/home/home';
+import { TelaLogin } from './components/tela-login/tela-login';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'listagem-iniciativas',
     component: ListagemIniciativas
+  },
+  {
+    path: 'login',
+    component: TelaLogin
   }
 ];
