c830e7606e87128505c39d9ea3e698dd2e8e6b40
Andrey <andreymarques958@gmail.com>
2026-08-26 15:07:11 -0300

    feat:  controle de acesso e criação de iniciativas e projetos com base no papel do usuário ( falta corrigir problemas com hash de senhas de perfis criados pela api e painel admin)

--- estrategia-api/projetosEstrategicos/views.py
+++ estrategia-api/projetosEstrategicos/views.py
@@ -6,3 +6,19 @@ class ProjetoEstrategicoViewSet(ModelViewSet):
     queryset = ProjetoEstrategico.objects.all()
     serializer_class = ProjetoEstrategicoSerializer 
     
+    def get_queryset(self):
+        usuario = self.request.user
+        
+        if usuario.pape == 'ADMIN':
+            return ProjetoEstrategico.objects.all()
+        
+        return ProjetoEstrategico.objects.filter(
+            unidade = usuario.unidade
+        )
+    
+    def perform_create(self, serializer):
+        usuario = self.request.user
+        if usuario.papel == 'ADMIN':
+            serializer.save()
+        else:
+            serializer.save(unidade = usuario.unidade, responsavel = usuario)
--- estrategia-api/usuarios/models.py
+++ estrategia-api/usuarios/models.py
@@ -8,8 +8,8 @@ class Usuario(AbstractUser):
         ('SERVIDOR', 'Servidor'),
     ]
     nome_completo = models.CharField(max_length=255)
-    papel = models.CharField(max_length=10, choices = PAPEL_CHOICES, default ='SERVIDOR ')
-    unidade = models.ForeignKey('unidades.Unidade', on_delete=models.SET_NULL, null=True,blank=True, related_name='usuarios')
+    papel = models.CharField(max_length=10, choices = PAPEL_CHOICES, default ='SERVIDOR')
+    unidade = models.ForeignKey('unidades.Unidade', on_delete= models.PROTECT, null=False,blank=True, related_name='usuarios')
     class Meta:
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
