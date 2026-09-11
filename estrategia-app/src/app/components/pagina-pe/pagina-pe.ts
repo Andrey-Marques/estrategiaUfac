990887bfbfa651a0d921fb321c816c534a69b023
GabiAbacaxi <gabyolisan07@gmail.com>
2026-09-11 10:04:25 -0500

    feat: alterando área pública

--- estrategia-app/src/app/components/pagina-pe/pagina-pe.ts
+++ estrategia-app/src/app/components/pagina-pe/pagina-pe.ts
@@ -1,10 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-pe',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './pagina-pe.html',
   styleUrl: './pagina-pe.scss',
 })
-export class PaginaPe {}
+export class PaginaPe {
+  opcaoSelecionada = 'apresentacao'
+
+  titulos: any = {
+    apresentacao: 'O Planejamento Estratégico da Ufac',
+    etapas: 'Etapas de Elaboração',
+    projetos: 'Projetos Estratégicos',
+    planejamento: 'Planejamento e Gestão Estratégica 2024-2033',
+    planos: 'Planos Anteriores',
+    documentos: 'Documentos',
+    indicadores: 'Indicadores Estratégicos'
+  };
+
+  selecionarOpcao(opcao:string){
+    this.opcaoSelecionada = opcao;
+  }
+}
